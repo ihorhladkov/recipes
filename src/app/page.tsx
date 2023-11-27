8aa8f30d7532c154ffaa8668aa308d7ebda4e8ac
ihorhladkov <ihor.hladkov@gmail.com>
2023-11-27 12:08:15 +0200

    setup zustand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,94 @@
-import { Button } from "./_components/ui/button";
+"use client";
+import { BellRing, Check } from "lucide-react";
 
-export default async function Home() {
+import { cn } from "./lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./_components/ui/card";
+
+const notifications = [
+  {
+    title: "Your call has been confirmed.",
+    description: "1 hour ago",
+  },
+  {
+    title: "You have a new message!",
+    description: "1 hour ago",
+  },
+  {
+    title: "Your subscription is expiring soon!",
+    description: "2 hours ago",
+  },
+];
+import { Button } from "./_components/ui/button";
+import { useCountStore } from "~/store/store";
+
+export default function Home() {
+  const counter = useCountStore((state) => state.counter);
+  const increase = useCountStore((state) => state.increase);
+
+  console.log(counter);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <Button variant='ghost'>Button</Button>
+      <Button variant="ghost">Button</Button>
+      <h1>Books: {counter} </h1>
+      <CardDemo />
+      <Button onClick={increase}>Add amount</Button>
     </main>
   );
 }
 
+type CardProps = React.ComponentProps<typeof Card>;
+
+export function CardDemo({ className, ...props }: CardProps) {
+  return (
+    <Card className={cn("w-[380px]", className)} {...props}>
+      <CardHeader>
+        <CardTitle>Notifications</CardTitle>
+        <CardDescription>You have 3 unread messages.</CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className=" flex items-center space-x-4 rounded-md border p-4">
+          <BellRing />
+          <div className="flex-1 space-y-1">
+            <p className="text-sm font-medium leading-none">
+              Push Notifications
+            </p>
+            <p className="text-sm text-muted-foreground">
+              Send notifications to device.
+            </p>
+          </div>
+        </div>
+        <div>
+          {notifications.map((notification, index) => (
+            <div
+              key={index}
+              className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+            >
+              <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+              <div className="space-y-1">
+                <p className="text-sm font-medium leading-none">
+                  {notification.title}
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  {notification.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full">
+          <Check className="mr-2 h-4 w-4" /> Mark all as read
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
