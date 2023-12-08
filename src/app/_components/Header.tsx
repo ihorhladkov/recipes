d609a922c691d4f6d7b4de3e20720214455c910e
ihorhladkov <ihor.hladkov@gmail.com>
2023-12-08 12:06:59 +0200

    fixed navlinks

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,20 +1,24 @@
 import Link from "next/link";
 import { Modal } from "./Modal";
 import { Suspense } from "react";
-import { Button } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import { Loader2 } from "lucide-react";
+import { cn } from "../lib/utils";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between pb-4">
       <div className="flex items-center gap-4">
         <h2 className="text-slate-50">Logo</h2>
-        <Button variant="ghost">
-          <Link href={"/"}>Home</Link>
-        </Button>
-        <Button variant="ghost">
-          <Link href={"/recipes"}>Recipes</Link>
-        </Button>
+        <Link className={cn(buttonVariants({ variant: "ghost" }))} href={"/"}>
+          Home
+        </Link>
+        <Link
+          className={cn(buttonVariants({ variant: "ghost" }))}
+          href={"/recipes"}
+        >
+          Recipes
+        </Link>
       </div>
       <Suspense
         fallback={
